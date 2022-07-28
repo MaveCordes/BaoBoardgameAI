119c7c5c215a19c8355f0dc27461f910fc7ed814
MaveCordes <46426398+MaveCordes@users.noreply.github.com>
2022-07-28 09:50:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     name="easyAI",
     version=__version__,
     description="Simple Python Boardgame Bao powered by zulko easyAI",
-    long_description=open("README.rst").read(),
+    long_description=open("README.md").read(),
     license="",
     keywords="Bao AI game powered by easyAI",
     packages=find_packages(exclude="docs"),
-    install_requires=["numpy, time"],
+    install_requires=["numpy"],
 )
